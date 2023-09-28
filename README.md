a779667166d413a3d2b5787dabba8f75363c3a4f
nadjib-boum <nadjib.dev22@gmail.com>
2023-09-28 18:56:09 +0400

    first commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# MERN-based eCommerce App
+
+An eCommerce application built using the MERN (MongoDB, Express.js, React, Node.js). It provides a platform for selling products or services over the internet with a wide range of features.
+
+## Key Features
+
+- **User-Friendly Frontend:** The app offers a responsive and intuitive interface developed with React.js, ensuring an engaging shopping experience for customers.
+
+- **Product Management:** Admins can efficiently manage product details, images, pricing, and categorization.
+
+- **Shopping Cart:** Customers can easily add, review, and purchase items, with the cart retaining selections across sessions.
+
+- **Secure Payment Processing:** Integration with secure payment gateways (Stripe) ensures safe transactions.
+
+- **User Authentication:** User accounts, login, and profile management are secure and user-friendly.
+
+- **Order Management:** Users can view order history, track shipments, and receive updates. Admins can manage orders and update statuses.
+
+- **Search and Filters:** Robust search and filtering options help customers find products quickly.
+
+- **Reviews and Ratings:** Customers can leave product reviews and ratings.
+
+- **Inventory Management:** Inventory levels are automatically updated to prevent overselling.
+
+- **Responsive Design:** The app is optimized for various devices.
+
+## Conclusion
+
+A MERN-based eCommerce app provides businesses with a feature-rich platform to establish and expand their online presence while offering customers a seamless shopping experience.
---
+++ api/app.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+
+const ErrorHandler = require("./middleware/error");
+const app = express();
+
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "http://127.0.0.1:3000"],
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+app.use(cookieParser());
+app.use("/test", (req, res) => {
+  res.send("Hello world!");
+});
+
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+
+// config
+if (process.env.NODE_ENV !== "PRODUCTION") {
+  require("dotenv").config({
+    path: "config/.env",
+  });
+}
+
+// import routes
+const user = require("./controller/user");
+const shop = require("./controller/shop");
+const product = require("./controller/product");
+const event = require("./controller/event");
+const coupon = require("./controller/coupounCode");
+const payment = require("./controller/payment");
+const order = require("./controller/order");
+const conversation = require("./controller/conversation");
+const message = require("./controller/message");
+const withdraw = require("./controller/withdraw");
+
+app.use("/api/v2/user", user);
+app.use("/api/v2/conversation", conversation);
+app.use("/api/v2/message", message);
+app.use("/api/v2/order", order);
+app.use("/api/v2/shop", shop);
+app.use("/api/v2/product", product);
+app.use("/api/v2/event", event);
+app.use("/api/v2/coupon", coupon);
+app.use("/api/v2/payment", payment);
+app.use("/api/v2/withdraw", withdraw);
+app.use(ErrorHandler);
+
+app.get("/", (req, res) => {
+  res.send("Hello world!");
+});
+
+module.exports = app;
